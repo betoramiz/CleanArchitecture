a5dea73c74355fcc0c46e960bd502d5b2c6858c1
BETO\betor <maramirez10@gmail.com>
2025-02-15 18:41:59 -0600

    added repository pattern

---
+++ Backend.Infrastructure/Persistance/Repositories/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using Backend.Infrastructure.Common;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+
+namespace Backend.Infrastructure.Persistance.Repositories;
+
+public class Repository(IOptions<ConnectionStringOptions> options)
+{
+    private readonly IOptions<ConnectionStringOptions> _options = options;
+    
+    internal IDbConnection CreateConnection()
+    {
+        return new SqlConnection(_options.Value.ConnectionString);
+    }
+}
--- Backend.Infrastructure/Persistance/Repositories/SomeRepository.cs
+++ Backend.Infrastructure/Persistance/Repositories/SomeRepository.cs
@@ -1,11 +1,14 @@
 ﻿using Backend.Application.Common.Interfaces;
+using Backend.Infrastructure.Common;
+using Microsoft.Extensions.Options;
 
 namespace Backend.Infrastructure.Persistance.Repositories;
 
-public class SomeRepository: ISomeRepository
+public class SomeRepository(IOptions<ConnectionStringOptions> options) : Repository(options), ISomeRepository
 {
     public Task GetSomeDataAsync()
     {
+        using var connection = CreateConnection();
         throw new NotImplementedException();
     }
 }
