50ca94b2de6030061cb17328ab468426c82e9bb1
BETO\betor <maramirez10@gmail.com>
2025-02-15 18:27:25 -0600

    added dependeci injection to infrastructure layer

---
+++ Backend.Infrastructure/Common/ConnectionStringOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.Infrastructure.Common;
+
+public class ConnectionStringOptions
+{
+    public const string Option = "ConnectionString";
+    public string ConnectionString { get; set; } = string.Empty;
+}
--- Backend.Infrastructure/DependencyInjection.cs
+++ Backend.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
 using Backend.Infrastructure.Interceptors;
 using Backend.Application.Data;
+using Backend.Infrastructure.Common;
 using Backend.Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
@@ -12,17 +14,25 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<ConnectionStringOptions>(configuration.GetSection("ConnectionString"));
+        
+        services.AddPersistence(configuration);
+        
         services.AddScoped<ICleanArchitectureContext>(serviceProvider => serviceProvider.GetRequiredService<CleanArchContext>());
         services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
-        
+
+        return services;
+    }
+
+    private static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var thisAssembly = Assembly.GetExecutingAssembly().GetName().Name;
         var connectionString = configuration.GetConnectionString("Connection");
         services.AddDbContext<CleanArchContext>((sp, options) =>
         {
             options.AddInterceptors(sp.GetService<ISaveChangesInterceptor>());
-            options.UseSqlServer(connectionString, x => x.MigrationsAssembly("Backend.Infrastructure"));
+            options.UseSqlServer(connectionString, x => x.MigrationsAssembly(thisAssembly));
         });
-        
-        
 
         return services;
     }
